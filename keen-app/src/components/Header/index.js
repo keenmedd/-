d2139fcc9847792fc75015c0998b2c950fa60fda
keenmedd <84720112+keenmedd@users.noreply.github.com>
2021-06-13 13:44:18 +0300

    Fix pull request

--- keen-app/src/components/Header/index.js
+++ keen-app/src/components/Header/index.js
@@ -5,13 +5,18 @@ import { routes } from "../../consts";
 export default function Header() {
   return (
     <div className="header__container">
-      <Link to={routes.doctors} className="header__link">
-        Врачи
-      </Link>
       <Link to={routes.home} className="header__logo">
         KEEN MEDD
       </Link>
-      <div className="header__link">Отзывы?</div>
+      <Link to={routes.home} className="header__link">
+        Для врачей
+      </Link>
+      <Link to={routes.doctors} className="header__link">
+      Для пациентов
+      </Link>
+      <div className="header__link">
+        Для мед учереждений
+      </div>
     </div>
   );
 }
