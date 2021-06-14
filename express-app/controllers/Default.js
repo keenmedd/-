82508cea505c5a48757f99344922a95340297c2c
keenmedd <84720112+keenmedd@users.noreply.github.com>
2021-06-14 23:45:42 +0300

    Backend

--- express-app/controllers/Default.js
+++ express-app/controllers/Default.js
@@ -1,24 +1,81 @@
-'use strict';
+"use strict";
 
-var utils = require('../utils/writer.js');
-var Default = require('../service/DefaultService');
+var utils = require("../utils/writer.js");
+var Default = require("../service/DefaultService");
 
-module.exports.doctorDetailsGET = function doctorDetailsGET(req, res, next, id) {
-    Default.doctorDetailsGET(id)
-        .then(function(response) {
-            utils.writeJson(res, response);
-        })
-        .catch(function(response) {
-            utils.writeJson(res, response);
-        });
+module.exports.doctorDetailsGET = function doctorDetailsGET(
+  req,
+  res,
+  next,
+  id
+) {
+  Default.doctorDetailsGET(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
 };
 
-module.exports.doctorGET = function doctorGET(req, res, next, name, specialty, category, expirience, workplace, city, rating, feedbackAmount, secondaryVisitPercent) {
-    Default.doctorGET(name, specialty, category, expirience, workplace, city, rating, feedbackAmount, secondaryVisitPercent)
-        .then(function(response) {
-            utils.writeJson(res, response);
-        })
-        .catch(function(response) {
-            utils.writeJson(res, response);
-        });
+module.exports.doctorGET = function doctorGET(
+  req,
+  res,
+  next,
+  name,
+  specialty,
+  category,
+  expirience,
+  workplace,
+  city,
+  rating,
+  feedbackAmount,
+  secondaryVisitPercent
+) {
+  Default.doctorGET(
+    name,
+    specialty,
+    category,
+    expirience,
+    workplace,
+    city,
+    rating,
+    feedbackAmount,
+    secondaryVisitPercent
+  )
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+module.exports.doctorFeedbackGET = function doctorFeedbackGET(
+  req,
+  res,
+  next,
+  id
+) {
+  Default.doctorFeedbackGET(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.doctorFeedbackPOST = function doctorFeedbackPOST(
+  req,
+  res,
+  next,
+  id
+) {
+  Default.doctorFeedbackPOST(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
 };
--- express-app/service/DefaultService.js
+++ express-app/service/DefaultService.js
@@ -1,95 +1,49 @@
 "use strict";
 
+const { fake } = require("faker");
 var faker = require("faker");
 faker.locale = "ru";
 
-const specialities = ["Терапевт", "Педиатр", "Стомотолог", "Хирург", "Ортодонт", "Офтальмолог", "Подолог", "Реабилитолог", "Психолог"];
-const categories = ["Вторая квалификационная", "Первая квалификационная", "Высшая квалификационная"]
-const workplaces = ["Больница скорой помощи", "Поликлиника №38", "Поликлиника №35", "Детская поликлиника №13", "Поликлиника №5", "Поликлиника №6", "Поликлиника №10", "Поликлиника №1", "Поликлиника №12", "Поликлиника №39", "Поликлиника №3", "Поликлиника №8", "Медицинский центр 'Горизонт'", "ЛОДЭ", "Городская студенческая поликлиника №33", "Поликлиника №37"]
-    /**
-     * get doctor details
-     * By passing in the appropriate options, you can get doctor details
-     *
-     * id String pass an id to get doctor details
-     * returns List
-     **/
-exports.doctorDetailsGET = function(id) {
-    return new Promise(function(resolve, reject) {
-        var examples = {};
-        examples["application/json"] = [{
-                specialty: "Терапия",
-                comments: [{
-                        createdAt: "2021-05-30T18:28:19.684Z",
-                        rating: 7.7,
-                        id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
-                        text: "Восхитительная работа!",
-                    },
-                    {
-                        createdAt: "2021-05-30T18:28:19.684Z",
-                        rating: 7.7,
-                        id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
-                        text: "Восхитительная работа!",
-                    },
-                ],
-                education: [{
-                        authority: "БелМАПО",
-                        name: "Курсы кардилогии",
-                        finishedAt: "2021-05-30T18:28:19.684Z",
-                    },
-                    {
-                        authority: "БелМАПО",
-                        name: "Курсы кардилогии",
-                        finishedAt: "2021-05-30T18:28:19.684Z",
-                    },
-                ],
-                popularity: 3222,
-                name: "Куку Валентина Андреевна",
-                rating: 7.7,
-                id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
-                experience: 5,
-                workplace: "Минская ЦРБ",
-            },
-            {
-                specialty: "Терапия",
-                comments: [{
-                        createdAt: "2021-05-30T18:28:19.684Z",
-                        rating: 7.7,
-                        id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
-                        text: "Восхитительная работа!",
-                    },
-                    {
-                        createdAt: "2021-05-30T18:28:19.684Z",
-                        rating: 7.7,
-                        id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
-                        text: "Восхитительная работа!",
-                    },
-                ],
-                education: [{
-                        authority: "БелМАПО",
-                        name: "Курсы кардилогии",
-                        finishedAt: "2021-05-30T18:28:19.684Z",
-                    },
-                    {
-                        authority: "БелМАПО",
-                        name: "Курсы кардилогии",
-                        finishedAt: "2021-05-30T18:28:19.684Z",
-                    },
-                ],
-                popularity: 3222,
-                name: "Куку Валентина Андреевна",
-                rating: 7.7,
-                id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
-                experience: 5,
-                workplace: "Минская ЦРБ",
-            },
-        ];
-        if (Object.keys(examples).length > 0) {
-            resolve(examples[Object.keys(examples)[0]]);
-        } else {
-            resolve();
-        }
-    });
-};
+const specialities = [
+  "Терапевт",
+  "Педиатр",
+  "Стомотолог",
+  "Хирург",
+  "Ортодонт",
+  "Офтальмолог",
+  "Подолог",
+  "Реабилитолог",
+  "Психолог",
+];
+const categories = [
+  "Вторая квалификационная",
+  "Первая квалификационная",
+  "Высшая квалификационная",
+];
+const authorities = [
+  "Гродненский государственный университет",
+  "Минский государственный медицинский университет",
+  "Витебский государственный медицинский университет",
+  "Московский государственный медицинский университет",
+];
+const workplaces = [
+  "Больница скорой помощи",
+  "Поликлиника №38",
+  "Поликлиника №35",
+  "Детская поликлиника №13",
+  "Поликлиника №5",
+  "Поликлиника №6",
+  "Поликлиника №10",
+  "Поликлиника №1",
+  "Поликлиника №12",
+  "Поликлиника №39",
+  "Поликлиника №3",
+  "Поликлиника №8",
+  "Медицинский центр 'Горизонт'",
+  "ЛОДЭ",
+  "Городская студенческая поликлиника №33",
+  "Поликлиника №37",
+];
 
 /**
  * searches doctors
@@ -100,30 +54,110 @@ exports.doctorDetailsGET = function(id) {
  * expirience Integer pass a filter for looking up doctor (optional)
  * returns List
  **/
-exports.doctorGET = function(name, specialty, expirience) {
-    return new Promise(function(resolve, reject) {
-        const doctors = Array.from({ length: faker.datatype.number(5) },
-            (x, i) => ({
-                name: faker.name.findName(),
-                speciality: faker.helpers.randomize(specialities),
-                category: faker.helpers.randomize(categories),
-                expirience: faker.datatype.number({ min: 0, max: 50 }),
-                workplace: faker.helpers.randomize(workplaces),
-                city: "Минск",
-                popularity: faker.datatype.number({ min: 0, max: 5000 }),
-                rating: faker.datatype.number({ min: 0, max: 5 }),
-                image: faker.image.avatar(),
-                feedbackAmount: faker.datatype.number({ min: 0 }),
-                secondaryVisitPercent: faker.datatype.number({ min: 0, max: 100 }),
-                id: faker.datatype.uuid(),
-            })
-        );
-        var examples = {};
-        examples["application/json"] = doctors;
-        if (Object.keys(examples).length > 0) {
-            resolve(examples[Object.keys(examples)[0]]);
-        } else {
-            resolve();
-        }
-    });
+exports.doctorGET = function (name, specialty, expirience) {
+  return new Promise(function (resolve, reject) {
+    const doctors = Array.from(
+      { length: faker.datatype.number(10) },
+      (x, i) => ({
+        name: faker.name.findName(),
+        speciality: faker.helpers.randomize(specialities),
+        category: faker.helpers.randomize(categories),
+        yearsOfExperience: faker.datatype.number({ min: 0, max: 50 }),
+        workplace: faker.helpers.randomize(workplaces),
+        city: "Минск",
+        popularity: faker.datatype.number({ min: 0, max: 5000 }),
+        rating: faker.datatype.number({ min: 0, max: 5 }),
+        image: faker.image.avatar(),
+        feedbackAmount: faker.datatype.number({ min: 0 }),
+        secondaryVisitPercent: faker.datatype.number({ min: 0, max: 100 }),
+        id: faker.datatype.uuid(),
+      })
+    );
+    var examples = {};
+    examples["application/json"] = doctors;
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+};
+exports.doctorFeedbackGET = function (name, specialty, expirience) {
+  return new Promise(function (resolve, reject) {
+    const doctors = Array.from(
+      { length: faker.datatype.number(5) },
+      (x, i) => ({
+        rating: faker.datatype.number({ min: 0, max: 5 }),
+        punctuality: faker.datatype.number({ min: 0, max: 5 }),
+        politeness: faker.datatype.number({ min: 0, max: 5 }),
+        clearity: faker.datatype.number({ min: 0, max: 5 }),
+        isFirstVisit: faker.datatype.boolean(),
+        authorName: faker.name.findName(),
+        visitDate: faker.date.past(),
+        comment: faker.lorem.sentence(15),
+      })
+    );
+    var examples = {};
+    examples["application/json"] = doctors;
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+};
+exports.doctorFeedbackPOST = function (name, specialty, expirience) {
+  return new Promise(function (resolve, reject) {
+    resolve();
+  });
+};
+
+exports.doctorDetailsGET = function (name, specialty, expirience) {
+  return new Promise(function (resolve, reject) {
+    debugger;
+
+    const doctor = {
+      name: faker.name.findName(),
+      speciality: faker.helpers.randomize(specialities),
+      category: faker.helpers.randomize(categories),
+      yearsOfExperience: faker.datatype.number({ min: 0, max: 50 }),
+      workplace: faker.helpers.randomize(workplaces),
+      city: "Минск",
+      popularity: faker.datatype.number({ min: 0, max: 5000 }),
+      rating: faker.datatype.number({ min: 0, max: 5 }),
+      image: faker.image.avatar(),
+      feedbackAmount: faker.datatype.number({ min: 0 }),
+      secondaryVisitPercent: faker.datatype.number({ min: 0, max: 100 }),
+      id: faker.datatype.uuid(),
+      education: Array.from({ length: faker.datatype.number(10) }, (x, i) => ({
+        year: faker.datatype.number({ min: 1950, max: 2021 }),
+        authority: faker.helpers.randomize(authorities),
+        note: faker.lorem.words(3),
+      })),
+      workExperience: Array.from(
+        { length: faker.datatype.number(10) },
+        (x, i) => ({
+          start: faker.date.past(),
+          end: faker.date.past(),
+          position: faker.helpers.randomize(specialities),
+          workplace: faker.helpers.randomize(workplaces),
+          competencies: Array.from(
+            { length: faker.datatype.number(10) },
+            (x, i) => faker.lorem.sentence(7)
+          ),
+          extraInfo: Array.from({ length: faker.datatype.number(10) }, (x, i) =>
+            faker.lorem.sentence(6)
+          ),
+        })
+      ),
+    };
+
+    var examples = {};
+    examples["application/json"] = doctor;
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
 };
