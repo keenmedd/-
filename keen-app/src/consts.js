a4c7289de87a5e60c27580e25603f0aa4541a0e7
keenmedd <84720112+keenmedd@users.noreply.github.com>
2021-06-15 08:29:01 +0300

    add

--- keen-app/src/consts.js
+++ keen-app/src/consts.js
@@ -1,6 +1,8 @@
 export const rootApiUrl = "http://localhost:1188";
 export const routes = {
-    home: "/",
-    doctors: "/doctors",
-    doctor: "/doctor"
+  home: "/",
+  doctors: "/doctors",
+  doctor: "/doctor",
+  thank: "/thank",
+  addfeedback: "/addfeedback",
 };
